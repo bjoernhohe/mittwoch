1a63280516becafff18c522250d2de76bba8118d
bjoern <bjoernhohe@users.noreply.github.com>
2019-12-01 00:54:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mittwochs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Today;
+            var start = new DateTime(2015, 03, 25, 00, 0, 0);
+            int tage = (today.Date - start.Date).Days;
+            int wochen = tage / 7;
+            Console.WriteLine(wochen.ToString());
+            Console.ReadKey();
+        }
+    }
+}
